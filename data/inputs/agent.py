c7bcf133bc69da52c824d9a47a696438adffe533
balamir53 <yzt@DESKTOP-92EGMFF.localdomain>
2023-03-24 18:47:36 +0300

    delete extra from data folder

--- data/inputs/agent.py
+++ data/inputs/agent.py
@@ -57,7 +57,7 @@ class EvaluationAgent():
         register_env("ray", lambda config : IndependentLearnerAll(args, self.agents,mapChange=args.mapChange))
         ppo_agent = PPOTrainer(config=config, env="ray")
         # TODO :change this to relative path
-        ppo_agent.restore(checkpoint_path="/workspaces/Suru2022/data/inputs/model/checkpoint_001300/checkpoint-1300")
+        ppo_agent.restore(checkpoint_path="model/checkpoint_002250/checkpoint-2250")
        
         self.truck_pol = ppo_agent.get_policy('truck')
         self.tankl_pol = ppo_agent.get_policy('tankl')
