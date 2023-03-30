8bd9fa6ee271c5c363ddaa7d2d7620ac4ee10062
ebron01 <emreboran@hacettepe.edu.tr>
2023-03-30 17:37:58 +0000

    action mask corrected truck

--- src/multitraincadet-all.py
+++ src/multitraincadet-all.py
@@ -99,7 +99,7 @@ def main():
             },
             "config": config,
             "checkpoint_freq": 50,
-                "restore": "/workspaces/Suru2022/data/inputs/model/checkpoint_007800/checkpoint-7800",
+                "restore": "/workspaces/Suru2022/data/inputs/model/checkpoint_008050/checkpoint-8050",
         },
     #  },resume=True)
     })
